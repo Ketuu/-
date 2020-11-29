86da5f8a8433cee2c96e21d5c75b4e56f0a0d104
Spink_wh <983767361@qq.com>
2020-11-29 11:20:09 +0800

    Enter

--- static/data.js
+++ static/data.js
@@ -2,7 +2,7 @@ const VIDEOS = [
 	{
 		id: 0,
 		videoUrl: "./static/videos/demo1.mp4",
-		poster: "http://image.pearvideo.com/cont/20201104/cont-1705200-12501405.jpg",
+		poster: "./static/videos/poster.jpg",
 		likenum: 101,
 		author: "./static/image/a1.png",
 		playing: true,
@@ -11,7 +11,7 @@ const VIDEOS = [
 	{
 		id: 1,
 		videoUrl: "./static/videos/demo2.mp4",
-		poster: "http://image.pearvideo.com/cont/20201110/11881540-175126-1.png",
+		poster: "./static/videos/poster.jpg",
 		likenum: 68888,
 		author: "./static/image/a2.png",
 		playing: false,
@@ -20,7 +20,7 @@ const VIDEOS = [
 	{
 		id: 2,
 		videoUrl: "./static/videos/demo3.mp4",
-		poster: "http://image.pearvideo.com/cont/20201110/cont-1706213-12505001.jpg",
+		poster: "./static/videos/poster.jpg",
 		likenum: 790,
 		author: "./static/image/a3.png",
 		playing: false,
@@ -29,7 +29,7 @@ const VIDEOS = [
 	{
 		id: 3,
 		videoUrl: "./static/videos/demo4.mp4",
-		poster: "http://image.pearvideo.com/cont/20201110/11881540-175126-1.png",
+		poster: "./static/videos/poster.jpg",
 		likenum: 124334,
 		author: "./static/image/a4.png",
 		playing: false,
@@ -38,7 +38,7 @@ const VIDEOS = [
 	{
 		id: 4,
 		videoUrl: "./static/videos/demo5.mp4",
-		poster: "http://image.pearvideo.com/cont/20201110/cont-1706213-12505001.jpg",
+		poster: "./static/videos/poster.jpg",
 		likenum: 6676,
 		author: "./static/image/a1.png",
 		playing: false,
