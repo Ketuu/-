b56a1903bd030fff0cc4ab625d9ce603c09d733a
Spink_wh <983767361@qq.com>
2020-11-29 11:14:20 +0800

    Enter

---
+++ README.md
@@ -0,0 +1,14 @@
+# 使用vue3.0实现仿抖音app滚动播放
+
+## 
+通过swiper工具，实现了视频滚动效果
+
+``` javascript
+  <!-- swiper插件 -->
+  <link rel="stylesheet" href="./static/swiper/swiper-bundle.min.css">
+  <script src="./static/swiper/swiper-bundle.min.js"></script>
+```
+
+## 
+配合vue，实现了点击播放暂停和双击爱心效果  
+有兴趣的朋友可以参考然后自己试一下
---
+++ static/data.js
@@ -0,0 +1,47 @@
+const VIDEOS = [
+	{
+		id: 0,
+		videoUrl: "./static/videos/demo1.mp4",
+		poster: "http://image.pearvideo.com/cont/20201104/cont-1705200-12501405.jpg",
+		likenum: 101,
+		author: "./static/image/a1.png",
+		playing: true,
+		love: false
+	},
+	{
+		id: 1,
+		videoUrl: "./static/videos/demo2.mp4",
+		poster: "http://image.pearvideo.com/cont/20201110/11881540-175126-1.png",
+		likenum: 68888,
+		author: "./static/image/a2.png",
+		playing: false,
+		love: false
+	},
+	{
+		id: 2,
+		videoUrl: "./static/videos/demo3.mp4",
+		poster: "http://image.pearvideo.com/cont/20201110/cont-1706213-12505001.jpg",
+		likenum: 790,
+		author: "./static/image/a3.png",
+		playing: false,
+		love: false
+	},
+	{
+		id: 3,
+		videoUrl: "./static/videos/demo4.mp4",
+		poster: "http://image.pearvideo.com/cont/20201110/11881540-175126-1.png",
+		likenum: 124334,
+		author: "./static/image/a4.png",
+		playing: false,
+		love: false
+	},
+	{
+		id: 4,
+		videoUrl: "./static/videos/demo5.mp4",
+		poster: "http://image.pearvideo.com/cont/20201110/cont-1706213-12505001.jpg",
+		likenum: 6676,
+		author: "./static/image/a1.png",
+		playing: false,
+		love: false
+	},
+]
